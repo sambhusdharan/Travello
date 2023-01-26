0873bc1dfa5d07a0ca0170d49e076bf36bcc663f
unknown <sambhusdharan@gmail.com>
2023-01-26 12:37:08 +0530

    eleven

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'travelproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
